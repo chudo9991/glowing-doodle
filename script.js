bfb0233bcbd79931d16f42090d078d49549998d1
Алексей Панфилов <panfav40@gmail.com>
2022-03-17 22:31:37 +0300

    landing: button, big card and closing is ready

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let wrapper = document.querySelector('.cardGroup--wrapper');
+let landing = document.querySelector('#bigCard--landing');
 
 wrapper.addEventListener('click', (e) => {
     const  { target } = e;
     if (target.matches('.cardGroup--button')) {
         if (target.matches('#landing')) {
-            console.log('ландинг');
+            // console.log('ландинг');
+            landing.style.display = "flex";
+            landing.addEventListener("click", () => {
+                landing.style.display = "none";
+            })
+            
         } else if (target.matches('#site')) {
             console.log('site');
         } else if (target.matches('#seo')) {
